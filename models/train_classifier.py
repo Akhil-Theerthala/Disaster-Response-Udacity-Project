2b4f62656e26d070cf54a410744d108416549f08
Akhil Theerthala <akhiltvsn@gmail.com>
2022-08-18 13:07:07 +0530

    Added TrainClassifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,101 @@
 import sys
+import sqlite3
+import pickle
+from sqlalchemy import create_engine
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+import pandas as pd 
+import numpy as np
+import re
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import classification_report, confusion_matrix, r2_score
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.naive_bayes import MultinomialNB
+
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql("SELECT * FROM Figure8Messages", engine)
+    
+    x = df.message.tolist()
+    y = df.drop(columns=['message', 'original','genre'], axis=1)
+    
+    return x,y,y.columns
 
 
 def tokenize(text):
-    pass
+    text =  re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    tokens = word_tokenize(text)
+    
+    lemmatizer = WordNetLemmatizer()
+    stemmer = PorterStemmer()
+    stop = stopwords.words('english')
+    
+    clean_tokens = []
+    for token in tokens:
+        if token not in stop:
+            clean_token = stemmer.stem(lemmatizer.lemmatize(token))
+            clean_tokens.append(clean_token)
 
+    return clean_tokens
+
+def display_results(y_test, y_pred):
+    labels = np.unique(y_pred)
+    confusion_mat = confusion_matrix(y_test, y_pred, labels=labels)
+    accuracy = (y_pred == y_test).mean()
+
+    print("Labels:", labels)
+    print("Confusion Matrix:\n", confusion_mat)
+    print("Accuracy:", accuracy)
 
 def build_model():
-    pass
-
-
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    
+    parameters = {
+    'vect__max_df': [1, 3],
+    'tfidf__sublinear_tf': [True, False],
+    'tfidf__use_idf': (True, False),
+    'clf__estimator__n_estimators' : [125, 250],
+    'clf__estimator__min_samples_leaf': [2,3]
+    }    
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    
+    
+    return cv
+    
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    
+    # display_results(y_test, y_pred)
+    for i in range(Y_test.shape[1]):
+        print(Y_test.columns[i])
+        print('--------------------')
+        display_results(Y_test.iloc[:,i], y_pred[:,i])
+        print('\n')
 
 
 def save_model(model, model_filepath):
-    pass
+    filename = model_filepath
+    pickle.dump(model, open(filename, 'wb'))
+
 
 
 def main():
