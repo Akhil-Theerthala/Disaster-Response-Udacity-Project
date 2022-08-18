b0797aa1a9d660a0e5caa444e27e358b681eef0c
Akhil Theerthala <akhiltvsn@gmail.com>
2022-08-18 13:19:38 +0530

    Deploying the app

--- app/run.py
+++ app/run.py
@@ -8,8 +8,8 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
+import joblib
 
 
 app = Flask(__name__)
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('Figure8Messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,9 +39,40 @@ model = joblib.load("../models/your_model_name.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    
+    cat_names = df.columns[4:]
+    cat_counts = df[cat_names].sum()
+    
+    #setup category groupings
+    aid = ['medical_help', 'medical_products', 'death','search_and_rescue',
+           'security','military','missing_people', 'refugees','water',
+           'food','shelter','clothing','money','other_aid']
+    infrastructure = ['infrastructure_related','transport','buildings',
+                      'electricity','tools','hospitals','shops','aid_centers','other_infrastructure']
+    weather = ['weather_related','floods','storm','fire','earthquake','cold','other_weather']
+    general = ['related','request','offer','direct_report']
+
+    #count messages with a at least one classification in the category group
+    aid_df = df[df[aid] == 1]
+    aid_df = aid_df.dropna(axis=0, how='all')
+    aid_count = aid_df.sum(axis=1).count()
+
+    infrastructure_df = df[df[infrastructure] == 1]
+    infrastructure_df = infrastructure_df.dropna(axis=0, how='all')
+    inf_count = infrastructure_df.sum(axis=1).count()
+
+    weather_df = df[df[weather] == 1]
+    weather_df = weather_df.dropna(axis=0, how='all')
+    weather_count = weather_df.sum(axis=1).count()
+
+    general_df = df[df[general] == 1]
+    general_df = general_df.dropna(axis=0, how='all')
+    general_count = general_df.sum(axis=1).count()
+    
+    cat_group_names = ['Aid Related', 'Infrastructure Related', 'Weather Related', 'General']
+    cat_group_counts = [aid_count, inf_count, weather_count, general_count]
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -61,6 +92,42 @@ def index():
                 },
                 'xaxis': {
                     'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cat_names,
+                    y=cat_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Category Classifications',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cat_group_names,
+                    y=cat_group_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Classification of messages into groups of Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category Grouping"
                 }
             }
         }
@@ -93,7 +160,7 @@ def go():
 
 
 def main():
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3001, debug=True)
 
 
 if __name__ == '__main__':
